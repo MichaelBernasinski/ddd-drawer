ebdce8f1bf1cfa138a7c8f60ace0f0326c43b33f
mbernasinski <contact@mbernasinski.com>
2024-10-14 17:09:20 +0200

    refactor: Biome check (auto lint/format) result

--- src/graphs/ddd-class-diagram.graph.ts
+++ src/graphs/ddd-class-diagram.graph.ts
@@ -1,10 +1,10 @@
 /**
  * **class-diagram**
- * 
+ *
  * Markdown Class Diagram with [mermaidjs](https://github.com/mermaid-js/mermaid)
- * 
+ *
  * ## Example
- * 
+ *
  * ```mermaid
  *  classDiagram
  * 	namespace io-dragee-rules-relation {
@@ -17,66 +17,67 @@
  * 	}
  * 	DrageeOne --> DrageeTwo : field, constructor, method_param
  * 	DrageeOne --> DrageeThree : constructor, method_param
- * 
+ *
  * 	namespace io-dragee-rules-anotherPackage {
  * 		class DrageeThree {
  * 			<<ddd/value_object>>
  * 		}
  * 	}
  * ```
- * <br/> 
+ * <br/>
  * <img src="../../assets/ddd-class-diagram.png" alt="DDD Class Diagram">
- * 
+ *
  * @module Class Diagram
  */
-import type { Dependency, Dragee } from "@dragee-io/type/common";
+import type { Dependency, Dragee } from '@dragee-io/type/common';
 
 const drawHandlerClassDiagram = (dragees: Dragee[]) => {
-    if(!dragees?.length) return '';
-    const namespaceClassMap = Map.groupBy(dragees, (d => extractPackageName(d.name)));
+    if (!dragees?.length) return '';
+    const namespaceClassMap = Map.groupBy(dragees, d => extractPackageName(d.name));
     return `\`\`\`mermaid\n\tclassDiagram\n${drawNamespacesDragee(namespaceClassMap)}\`\`\``;
-}
+};
 
 const drawNamespacesDragee = (namespaceClassMap: Map<string, Dragee[]>): string => {
-    let namespaces = ``;
-    for(let [namespace, dragees] of namespaceClassMap.entries()) {
+    let namespaces = '';
+    for (const [namespace, dragees] of namespaceClassMap.entries()) {
         namespaces += drawNamespaceDragee(dragees, namespace);
     }
     return namespaces;
-}
+};
 
 const drawNamespaceDragee = (dragees: Dragee[], namespace: string): string => {
     return `\tnamespace ${namespace.replaceAll('.', '-')} {${dragees.map(drawClassDragee).join('')}\t}\n${dragees.map(drawDependenciesDragee).join('')}\n`;
-}
+};
 
 const drawClassDragee = (dragee: Dragee): string => {
     return `\n\t\tclass ${extractClassName(dragee.name)} {\n\t\t\t<<${dragee.profile}>>\n\t\t}\n`;
-}
+};
 
 const drawDependenciesDragee = (dragee: Dragee): string => {
     return `${Object.entries(dragee.depends_on)
-        .map(dependancy => drawDependencyDragee(extractClassName(dragee.name), dependancy)).join('')}`;
-}
+        .map(dependancy => drawDependencyDragee(extractClassName(dragee.name), dependancy))
+        .join('')}`;
+};
 
 const drawDependencyDragee = (drageeName: string, dependancy: [string, Dependency]): string => {
     return `\t${drageeName} --> ${extractClassName(dependancy[0])} : ${drawDependencyTypeDragee(dependancy[1])}\n`;
-}
+};
 
 const drawDependencyTypeDragee = (dependancy: Dependency): string => {
-    return `${Object.values(dependancy).join(", ")}`;
-}
+    return `${Object.values(dependancy).join(', ')}`;
+};
 
 const extractClassName = (drageeName: string): string => {
     return drageeName.split('.').pop() ?? '';
 };
 
 const extractPackageName = (drageeName: string): string => {
-    const drageeNameSplit = drageeName.split('.')
+    const drageeNameSplit = drageeName.split('.');
     drageeNameSplit.pop();
-    return drageeNameSplit.join('.')
+    return drageeNameSplit.join('.');
 };
 
 export default {
     label: 'Class Diagram',
     handler: drawHandlerClassDiagram
-}
+};
--- test/ddd-class-diagram.graph.spec.ts
+++ test/ddd-class-diagram.graph.spec.ts
@@ -1,6 +1,6 @@
-import { describe, expect, test } from "bun:test";
-import type { Dragee } from "@dragee-io/type/common";
-import dddClassDiagram from "..";
+import { describe, expect, test } from 'bun:test';
+import type { Dragee } from '@dragee-io/type/common';
+import dddClassDiagram from '..';
 
 const graphId = 'ddd/class-diagram';
 const classDiagramGraph = dddClassDiagram.graphs.filter(graph => graph.id === graphId)[0];
@@ -9,20 +9,20 @@ describe('Should display correct DDD class diagram', () => {
     test('with no dragees', async () => {
         const dragees: Dragee[] = [];
         const graphResult = classDiagramGraph.handler(dragees);
-        
-        expect(graphResult).toBeEmpty();  
-    })
+
+        expect(graphResult).toBeEmpty();
+    });
 
     test('with dragees', async () => {
-        const drageeOne: Dragee = await Bun.file("./test/sample/drageeOne.json").json();
-        const drageeTwo: Dragee = await Bun.file("./test/sample/drageeTwo.json").json();
-        const drageeThree: Dragee = await Bun.file("./test/sample/drageeThree.json").json();
+        const drageeOne: Dragee = await Bun.file('./test/sample/drageeOne.json').json();
+        const drageeTwo: Dragee = await Bun.file('./test/sample/drageeTwo.json').json();
+        const drageeThree: Dragee = await Bun.file('./test/sample/drageeThree.json').json();
         const dragees: Dragee[] = [drageeOne, drageeTwo, drageeThree];
 
         const graphResult = classDiagramGraph.handler(dragees);
 
-        const testExpectedResult = await Bun.file("./test/expected/testExpectedResult.md").text();
+        const testExpectedResult = await Bun.file('./test/expected/testExpectedResult.md').text();
         expect(graphResult).not.toBeEmpty();
         expect(graphResult).toBe(testExpectedResult);
-    })
-})
+    });
+});
